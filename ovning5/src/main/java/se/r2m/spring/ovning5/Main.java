e35c0fbaf56289f4ce71ecc70d1f4df110906a62
Bjorn Frantzen <bjorn.frantzen@cuemind.com>
2014-08-22 11:40:17 +0200

    Bättre context

--- ovning5/src/main/java/se/r2m/spring/ovning5/Main.java
+++ ovning5/src/main/java/se/r2m/spring/ovning5/Main.java
@@ -3,6 +3,9 @@ package se.r2m.spring.ovning5;
 import java.io.File;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.Host;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.ContextConfig;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,14 +14,27 @@ import se.r2m.spring.ovning5.servlet.ListSpringBeansServlet;
 public class Main {
 
     public static void main(final String[] args) throws Exception {
-		try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("se")) {
-	        Tomcat tomcat = new Tomcat();
-	        tomcat.setPort( 8080 );
-	        Context context = tomcat.addContext( "/", new File("WebContent/").getAbsolutePath() );
-	        Tomcat.addServlet( context, "Simple", new ListSpringBeansServlet(ctx) );
-	        context.addServletMapping( "/*", "Simple" );
-	        tomcat.start();
-	        tomcat.getServer().await();
-		}
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort( 8080 );
+        Context context = addContext( tomcat.getHost(), "/", new File("WebContent/").getAbsolutePath() );
+    	AnnotationConfigApplicationContext springctx = new AnnotationConfigApplicationContext("se");
+        Tomcat.addServlet( context, "Simple", new ListSpringBeansServlet(springctx) );
+        context.addServletMapping( "/*", "Simple" );
+        tomcat.start();
+        tomcat.getServer().await();
 	}
+    
+    public static Context addContext(Host host, String contextPath, String dir) {
+        Context context = null;
+        context = new StandardContext();
+        context.setName(contextPath);
+        context.setPath(contextPath);
+        context.setDocBase(dir);
+        context.setRealm(host.getRealm());
+        ContextConfig contextConfig = new ContextConfig();
+        context.addLifecycleListener(contextConfig);
+        host.addChild(context);
+        return context;
+    }
+
 }
